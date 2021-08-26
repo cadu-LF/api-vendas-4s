2a61b2416af905b7bcdd428d9cad64ed06e7791e
Carlos Eduardo Lourenço <nice.ice312@gmail.com>
2021-08-25 21:46:34 -0300

    25-08

---
+++ src/modules/Products/Services/UpdateProductService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "../../../shared/Errors/AppErrors";
+import Product from "../typeorm/entities/Product";
+import ProductRepository from "../typeorm/Repositories/ProductRepository";
+
+interface IRequest{
+  id: string,
+  name: string,
+  price: number,
+  quantity: number
+}
+
+export default class UpdateProductService{
+  public async execute({id, name, price, quantity}: IRequest): Promise<Product>{
+    let productRepository = getCustomRepository(ProductRepository)
+
+    let productExists = await productRepository.findOne(id)
+
+    if(!productExists){
+      // lança uma excecao
+      throw new AppError('Produto não existe')
+    }
+
+    let productSameName = await productRepository.findByName(name);
+    if(!productSameName){
+      throw new AppError('Produto já tem um nome deste')
+    }
+
+    // dar o update
+    productExists.name = name
+    productExists.price = price
+    productExists.quantity = quantity
+    await productRepository.save(productExists); // como o productExiste tem id, ele atualiza
+    return productExists;
+  }
+}
