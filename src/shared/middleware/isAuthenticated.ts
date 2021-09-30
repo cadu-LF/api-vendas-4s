9cced7b94d2559f194559f71a9373ab8f13b41fb
Carlos Eduardo Lourenço <nice.ice312@gmail.com>
2021-09-29 22:13:57 -0300

    29-09

--- src/shared/middleware/isAuthenticated.ts
+++ src/shared/middleware/isAuthenticated.ts
@@ -3,6 +3,13 @@ import { verify } from "jsonwebtoken"
 import AppError from "../Errors/AppErrors"
 import authConfig from "../../config/auth"
 
+// interface que é um tipo de dado contendo os campos do token
+interface ITokenPayload {
+    iat: number,
+    exp: number,
+    sub: string
+}
+
 export default function isAuthenticated(
   request: Request, 
   response: Response, 
@@ -26,7 +33,18 @@ export default function isAuthenticated(
 
     // tratamento de erro
     try { // tenta verificar se o token é valido
+      /**
+       * O token decodificado gerado tem a estrutura:
+       *    iat: momento em timestamp que o token começa a funcionar
+       *    exp: momento em timestamp que o token para de funcionar
+       *    sub: armazena as informações do id do usuário, que vamos pegar na request
+       */
       let decodedToken = verify(token, authConfig.jwt.secret)
+
+      const {sub} =  decodedToken as ITokenPayload // pega apenas o sub do decodedToken
+
+      // vamos alterar as informações da request, 
+      // pq a requisição deve conter o id do usuário que está no sub
       return next() //deixa a API ser consumida
     }
     catch { // lança erro se o token não for válido
